cd8302e52305be8c8692d3405f75b30346ed3933
FarkhadTut <74925753+FarkhadTut@users.noreply.github.com>
2022-08-05 21:55:28 +0500

    Update admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from products.models import *
+from .models import *
 from django.forms import Textarea
 from nested_admin import NestedModelAdmin, NestedTabularInline, NestedStackedInline
 from django.forms import TextInput, Textarea
